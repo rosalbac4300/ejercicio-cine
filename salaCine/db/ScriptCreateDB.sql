b7e4b36bd8835ff68e51ea8e5e7cbe3032c2b3f5
Rosalba Caldez <rochu.caldez@gmail.com>
2025-11-19 10:04:34 -0300

    MOdificar script

--- salaCine/db/ScriptCreateDB.sql
+++ salaCine/db/ScriptCreateDB.sql
@@ -2,15 +2,17 @@ CREATE SCHEMA IF NOT EXISTS `calendario-cine`;
 USE `calendario-cine`;
 
 -- CREACION TABLAS
-DROP TABLE IF EXISTS `Genero`;
-CREATE TABLE `Genero` (
+CREATE TABLE IF NOT EXISTS `Genero` (
   `IdGenero` SMALLINT NOT NULL AUTO_INCREMENT,
 
   `Genero` VARCHAR(50) NOT NULL,
   `Estado` CHAR(1) NOT NULL,
 
   PRIMARY KEY (`IdGenero`),
-  UNIQUE INDEX `Genero-Genero-UX` (`Genero`) VISIBLE
+  UNIQUE INDEX `Genero-Genero-UX` (`Genero`) VISIBLE,
+
+  -- Todos los campos Estado deben ser 'A' o 'I'
+  CONSTRAINT `Genero-Estado-CHK` CHECK (`Estado` IN ('A', 'I'))
 );
 
 CREATE TABLE IF NOT EXISTS `Sala` (
@@ -23,11 +25,12 @@ CREATE TABLE IF NOT EXISTS `Sala` (
   `Observaciones` VARCHAR(255) NULL,
 
   PRIMARY KEY (`IdSala`),
-  UNIQUE INDEX `Sala-Sala-UX` (`Sala`) VISIBLE
+  UNIQUE INDEX `Sala-Sala-UX` (`Sala`) VISIBLE,
+  -- Todos los campos Estado deben ser 'A' o 'I'
+  CONSTRAINT `Sala-Estado-CHK` CHECK (`Estado` IN ('A', 'I'))
 );
 
-DROP TABLE IF EXISTS `Butaca`;
-CREATE TABLE `Butaca` (
+CREATE TABLE IF NOT EXISTS `Butaca` (
 	`IdButaca` INT NOT NULL AUTO_INCREMENT,
 	`IdSala` SMALLINT NOT NULL,
 
@@ -40,11 +43,18 @@ CREATE TABLE `Butaca` (
     PRIMARY KEY (`IdButaca`, `IdSala`),
     FOREIGN KEY (`IdSala`) REFERENCES `Sala`(`IdSala`),
     UNIQUE INDEX `Butaca-idButaca-UX` (`IdButaca`),
-    UNIQUE INDEX `Butaca-NroButacaIdSala-UX` (`NroButaca`, `IdSala`)
+    UNIQUE INDEX `Butaca-NroButacaIdSala-UX` (`NroButaca`, `IdSala`),
+
+    -- Todos los campos Estado deben ser 'A' o 'I'
+    CONSTRAINT `Butaca-Estado-CHK` CHECK (`Estado` IN ('A', 'I')),
+    -- Nombre butaca
+    UNIQUE INDEX `Butaca-FilaColumna-UX` (`IdSala`, `Columna`, `Fila`),
+
+    -- Indices
+    INDEX `Butaca-IdSalaNroButaca-IX` (`IdSala`, `NroButaca`)
 );
 
-DROP TABLE IF EXISTS `Pelicula`;
-CREATE TABLE `Pelicula` (
+CREATE TABLE IF NOT EXISTS `Pelicula` (
 	`IdPelicula` INT NOT NULL AUTO_INCREMENT,
     `IdGenero` SMALLINT NOT NULL,
 
@@ -58,11 +68,15 @@ CREATE TABLE `Pelicula` (
 
     PRIMARY KEY (`IdPelicula`),
     UNIQUE INDEX `Pelicula-Pelicula-UX` (`Pelicula`),
-    FOREIGN KEY (`IdGenero`) REFERENCES `Genero`(`IdGenero`)
+    FOREIGN KEY (`IdGenero`) REFERENCES `Genero`(`IdGenero`),
+
+    -- Todos los campos Estado deben ser 'A' o 'I'
+    CONSTRAINT `Pelicula-Estado-CHK` CHECK (`Estado` IN ('A', 'I')),
+    -- Duracion
+    CONSTRAINT `Pelicula-Duracion-CHK` CHECK (`Duracion` > 0)
 );
 
-DROP TABLE IF EXISTS `Funcion`;
-CREATE TABLE `Funcion` (
+CREATE TABLE IF NOT EXISTS `Funcion` (
 	`IdFuncion` INT NOT NULL UNIQUE AUTO_INCREMENT,
     `IdPelicula` INT NOT NULL,
 	`IdSala` SMALLINT NOT NULL,
@@ -77,11 +91,21 @@ CREATE TABLE `Funcion` (
 
     PRIMARY KEY (`IdFuncion`, `IdPelicula`, `IdSala`),
     FOREIGN KEY (`IdPelicula`) REFERENCES `Pelicula`(`IdPelicula`),
-    FOREIGN KEY (`IdSala`) REFERENCES `Sala`(`IdSala`)
+    FOREIGN KEY (`IdSala`) REFERENCES `Sala`(`IdSala`),
+
+    -- Campo Precio debe ser Mayor a Cero
+	CONSTRAINT `Funcion-Precio-CHK` CHECK (`Precio` > 0),
+    -- Todos los campos Estado deben ser 'A' o 'I'
+    CONSTRAINT `Funcion-Estado-CHK` CHECK (`Estado` IN ('A', 'I')),
+    -- Fechas
+    CONSTRAINT `Funcion-Fecha-CHK` CHECK (`FechaInicio` < `FechaFin`),
+    CONSTRAINT `Funcion-FechaProbable-CHK` CHECK (`FechaProbableInicio` <= `FechaProbableFin`),
+
+    -- Indices
+    INDEX `Funcion-FechaInicio-IX` (`FechaInicio`)
 );
 
-DROP TABLE IF EXISTS `Reserva`;
-CREATE TABLE `Reserva` (
+CREATE TABLE IF NOT EXISTS `Reserva` (
 	`IdReserva` BIGINT UNIQUE AUTO_INCREMENT,
     `IdFuncion` INT NOT NULL,
     `IdPelicula` INT NOT NULL,
@@ -98,58 +122,8 @@ CREATE TABLE `Reserva` (
     FOREIGN KEY (`IdFuncion`) REFERENCES `Funcion`(`IdFuncion`),
     FOREIGN KEY (`IdPelicula`) REFERENCES `Pelicula`(`IdPelicula`),
     FOREIGN KEY (`IdSala`) REFERENCES `Sala`(`IdSala`),
-    FOREIGN KEY (`IdButaca`) REFERENCES `Butaca`(`IdButaca`)
+    FOREIGN KEY (`IdButaca`) REFERENCES `Butaca`(`IdButaca`),
+    
+    -- Indices
+    INDEX `Reserva-IdFuncionIdButaca-IX` (`IdFuncion`, `IdReserva`)
 );
-
--- CONSTRAINTS
--- Campo Precio debe ser Mayor a Cero
-ALTER TABLE `Funcion`
-	ADD CONSTRAINT `Funcion-Precio-CHK` CHECK (`Precio` > 0);
-
--- Todos los campos Estado deben ser 'A' o 'I'
-ALTER TABLE `Genero`
-	ADD CONSTRAINT `Genero-Estado-CHK` CHECK (`Estado` IN ('A', 'I'));
-
-ALTER TABLE `Sala`
-	ADD CONSTRAINT `Sala-Estado-CHK` CHECK (`Estado` IN ('A', 'I'));
-
-ALTER TABLE `Butaca`
-	ADD CONSTRAINT `Butaca-Estado-CHK` CHECK (`Estado` IN ('A', 'I'));
-
-ALTER TABLE `Pelicula`
-	ADD CONSTRAINT `Pelicula-Estado-CHK` CHECK (`Estado` IN ('A', 'I'));
-
-ALTER TABLE `Funcion`
-	ADD CONSTRAINT `Funcion-Estado-CHK` CHECK (`Estado` IN ('A', 'I'));
-
--- Constraints de fechas
-ALTER TABLE `Funcion`
-		ADD CONSTRAINT `Funcion-Fecha-CHK` CHECK (`FechaInicio` < `FechaFin`);
-
-ALTER TABLE `Funcion`
-		ADD CONSTRAINT `Funcion-FechaProbable-CHK` CHECK (`FechaProbableInicio` <= `FechaProbableFin`);
-
--- Constraints para nombres de butacas
-ALTER TABLE `Butaca`
-	ADD UNIQUE INDEX `Butaca-FilaColumna-UX` (`IdSala`, `Columna`, `Fila`);
-
--- Agrego constraint de duracion de la pelicula:
-ALTER TABLE `Pelicula`
-	ADD CONSTRAINT `Pelicula-Duracion-CHK` CHECK (`Duracion` > 0);
-
--- Agregar constraint de valor EstaPagada?
-
--- INDICES
-CREATE INDEX `Funcion-FechaInicio-IX`
-	ON `Funcion`
-	(`FechaInicio`);
-
-CREATE INDEX `Butaca-IdSalaNroButaca-IX`
-	ON `Butaca`
-	(`IdSala`, `NroButaca`);
-
-CREATE INDEX `Reserva-IdFuncionIdButaca-IX`
-	ON `Reserva`
-	(`IdFuncion`, `IdButaca`);
-
--- Otros indices Necesarios:
